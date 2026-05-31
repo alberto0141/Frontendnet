b55be073c1c28d3560ac8daeac5c4695dfff55ee
¨alberto0141¨ <zS23021469@estudiantes.uv.mx>
2026-05-31 15:45:46 -0600

    Primer commit

---
+++ Models/AuthUser.cs
@@ -0,0 +1,10 @@
+namespace frontendnet.Models
+{
+    public class AuthUser
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Rol { get; set; }
+        public string? Jwt { get; set; }
+    }
+}
